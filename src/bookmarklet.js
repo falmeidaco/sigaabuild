d0cfd5923164be58d3642b39eff4fcd069e566a2
Felipe Almeida <falmeidaco@Felipe-Almeida.local>
2019-01-17 21:10:44 -0300

    remote file

---
+++ src/bookmarklet.js
@@ -0,0 +1,6 @@
+(function(d) {
+    var e = d.createElement('script');
+    e.async = true;
+    e.src = 'https://falmeidaco.github.io/sigaabuild/src/script.js';
+    d.getElementsByTagName('body')[0].appendChild(e);
+}(document));
