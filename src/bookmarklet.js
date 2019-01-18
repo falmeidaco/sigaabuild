3495ac65f5a0dff6a9cb96d836950f7ce7318540
Felipe Almeida <falmeidaco@Felipe-Almeida.local>
2019-01-17 22:00:18 -0300

    stylesheet remote; cel with notification icon;

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,6 +1,10 @@
 (function(d) {
-    var e = d.createElement('script'), t = new Date();
+    var s = d.createElement('link'), e = d.createElement('script'), t = new Date();
     e.async = true;
     e.src = 'https://falmeidaco.github.io/sigaabuild/src/script.js?v='+t.getTime();
     d.getElementsByTagName('body')[0].appendChild(e);
+    s.type = "text/css";
+    s.rel = "stylesheet";
+    s.href = 'https://falmeidaco.github.io/sigaabuild/src/style.css?v='+t.getTime();
+    d.getElementsByTagName('head')[0].appendChild(s);
 }(document));
