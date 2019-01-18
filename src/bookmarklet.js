60999f6c16daaf8387d7a4dfdcd27122b154079f
Felipe Almeida <falmeidaco@Felipe-Almeida.local>
2019-01-17 21:14:28 -0300

    force script cache

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,6 +1,6 @@
 (function(d) {
-    var e = d.createElement('script');
+    var e = d.createElement('script'), t = new Date();
     e.async = true;
-    e.src = 'https://falmeidaco.github.io/sigaabuild/src/script.js';
+    e.src = 'https://falmeidaco.github.io/sigaabuild/src/script.js?v='+t.getTime();
     d.getElementsByTagName('body')[0].appendChild(e);
 }(document));
